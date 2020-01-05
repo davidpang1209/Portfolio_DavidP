e81ceb6835a1c53333c185b9916bb0b107a20dd6
deykpe <dpang2@uci.edu>
2020-01-04 21:50:12 -0800

    updated css

--- js/script.js
+++ js/script.js
@@ -64,6 +64,23 @@ $(document).ready(function(){ //jquery
             easing: 'linear',
             queue: false
         }
+    });
+
+    $('#filters a').click(function() {
+        $('#filters .current').removeClass ('current');
+        $(this).addClass('current');
+        
+        var selector = $(this).attr('data-filter');
+
+        $('.items').isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        });
+            return false;
     })
 });
 
