68608b12801ba536975b9941ee7be93615bbc699
deykpe <dpang2@uci.edu>
2020-01-04 21:37:31 -0800

    styled/added/zoomed

--- js/script.js
+++ js/script.js
@@ -55,5 +55,15 @@ $(document).ready(function(){ //jquery
         }
     });
    
+    $('[data-fancybox]').fancybox();
+
+    $('.items').isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    })
 });
 
