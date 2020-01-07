ac61937fa3db949e0b0562d0f89aecd96ff77d47
deykpe <dpang2@uci.edu>
2020-01-06 18:27:41 -0800

    sticking nav bar on scroll

--- js/script.js
+++ js/script.js
@@ -82,5 +82,19 @@ $(document).ready(function(){ //jquery
         });
             return false;
     })
-});
 
+    const nav = $ ('#navigation');
+    const navTop = nav.offset().top;
+    $(window).on('scroll', stickyNavigation);
+    function stickyNavigation () {
+        const body = $('body');
+        if ($(window).scrollTop() <= navTop) {
+            body.addClass('fixedNav');
+        }
+        else {
+            body.removeClass('fixedNav');
+        }
+    }
+ 
+});
+ 
